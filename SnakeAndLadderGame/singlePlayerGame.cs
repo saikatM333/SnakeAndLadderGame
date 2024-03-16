d7184564d1e11173e39df57584bf1953f5370d6a
saikatCV <saikat030703@gmail.com>
2024-03-16 08:41:06 +0530

    UC2 : created the function for rolling the dice

--- SnakeAndLadderGame/singlePlayerGame.cs
+++ SnakeAndLadderGame/singlePlayerGame.cs
@@ -8,7 +8,27 @@ namespace SnakeAndLadderGame
 {
     public  class singlePlayerGame
     {
+        public string name;        
+        public singlePlayerGame()
+        {
+            Console.WriteLine("Enter the name\n ");
+            string name = Console.ReadLine();
+            this.name = name;
+            
 
+        
+        }
+
+        public void playingTheGame()
+        {
+            // roll the dice and it will give the value between 1 - 6
+            Random r = new Random();
+            int roll = r.Next(1, 7);
+           
+            int count = 0;
+
+           
+        }
 
     }
 }
